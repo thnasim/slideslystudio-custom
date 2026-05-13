249ba5c3933c1a0a38545f28a3eb195b1f3c974a
Tanvir Hasan <metanvir01@gmail.com>
2026-05-13 23:25:50 +0600

    Update page.tsx

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,15 +1,9 @@
 'use client';
 
-/**
- * This route mounts the Sanity Studio at /studio.
- * Visit /studio in dev or production to manage content.
- */
-
 import { NextStudio } from 'next-sanity/studio';
 import config from '../../../../sanity.config';
 
 export const dynamic = 'force-static';
-export { metadata, viewport } from 'next-sanity/studio';
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
