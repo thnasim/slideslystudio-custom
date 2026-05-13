4a7d2f576667cccb7168056565066a34025d1dd8
Tanvir Hasan <metanvir01@gmail.com>
2026-05-13 23:53:03 +0600

    Update page.tsx

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,10 +1,9 @@
-'use client';
+import Studio from './Studio';
 
-import { NextStudio } from 'next-sanity/studio';
-import config from '../../../../sanity.config';
+export { metadata, viewport } from 'next-sanity/studio';
 
 export const dynamic = 'force-static';
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <Studio />;
 }
